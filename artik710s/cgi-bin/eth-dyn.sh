ac3d0bab4dcc4510052352951f003a2c17690074
Min-ho Park <parkmino@gmail.com>
2018-09-03 12:27:04 +0900

    Initialize all static ip for dhcp

--- artik710s/cgi-bin/eth-dyn.sh
+++ artik710s/cgi-bin/eth-dyn.sh
@@ -10,6 +10,8 @@ cat /var/www/html/return.html
 #[ -n "$(which connmanctl)" ] && eth=$(connmanctl services | grep -om1 ethernet.*)
 #connmanctl config $eth --ipv4 dhcp
 
-sudo sed -i '/ip4/s/value=".*"/value=""/' /var/www/html/index.html
+for i in ip1 ip2 ip3 ip4; do
+ sudo sed -i '/'"$i"'/s/value=".*"/value=""/' /var/www/html/index.html
+done
 
 echo '<meta http-equiv="refresh" content="3;url=/index.html">'
