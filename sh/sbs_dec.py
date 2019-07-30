1622e4f4437d714a6b02d271b157a373d3db4aa8
Min-ho Park <parkmino@gmail.com>
2019-07-31 07:36:27 +0900

    Use python cyptodome in place of crypto

--- sh/sbs_dec.py
+++ sh/sbs_dec.py
@@ -4,11 +4,11 @@
 
 import sys
 import base64
-import Crypto.Cipher.DES
+import Cryptodome.Cipher.DES
 
 data = sys.argv[1]
 key = b'7d1ff4ea8925c225'
 ciphertext = base64.b64decode(data)
-cipher = Crypto.Cipher.DES.new(key[:8], mode=Crypto.Cipher.DES.MODE_ECB)
+cipher = Cryptodome.Cipher.DES.new(key[:8], mode=Cryptodome.Cipher.DES.MODE_ECB)
 text = cipher.decrypt(ciphertext)
 print(text.decode('utf8'))
