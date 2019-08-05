d7027190ff46e2b9dfe134c028ec8436d247d908
Min-ho Park <parkmino@gmail.com>
2019-08-06 06:59:26 +0900

    Properly unpad PKCS5

--- raspi/sbs_dec.py
+++ raspi/sbs_dec.py
@@ -10,5 +10,6 @@ data = sys.argv[1]
 key = b'7d1ff4ea8925c225'
 ciphertext = base64.b64decode(data)
 cipher = Cryptodome.Cipher.DES.new(key[:8], mode=Cryptodome.Cipher.DES.MODE_ECB)
-text = cipher.decrypt(ciphertext)
+unpad = lambda s : s[0:-ord(s[-1])]
+text = unpad(cipher.decrypt(ciphertext))
 print(text.decode('utf8'))
