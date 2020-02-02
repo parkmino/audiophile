a0d44816054c4ccff295e67a27d6c790829446b5
Min-ho Park <parkmino@gmail.com>
2020-02-02 10:16:58 +0900

    Compatible with python3

--- sh/sbs_dec.py
+++ sh/sbs_dec.py
@@ -10,6 +10,6 @@ data = sys.argv[1]
 key = b'7d1ff4ea8925c225'
 ciphertext = base64.b64decode(data)
 cipher = Cryptodome.Cipher.DES.new(key[:8], mode=Cryptodome.Cipher.DES.MODE_ECB)
-unpad = lambda s : s[0:-ord(s[-1])]
+unpad = lambda s : s[0:-ord(s[-1:])]
 text = unpad(cipher.decrypt(ciphertext))
 print(text.decode('utf8'))
