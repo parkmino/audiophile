a09b42db6af2144c9f47f52f70a17d3320a23674
Min-ho Park <parkmino@gmail.com>
2020-03-29 15:26:32 +0900

    Replace pcm.0

--- raspi/patch_20.04.sh
+++ raspi/patch_20.04.sh
@@ -6,7 +6,7 @@ alsa_conf=/usr/share/alsa/alsa.conf
 mpd_conf=/etc/mpd.conf.sav
 rc_local=/etc/rc.local
 
-sudo sed -i '/defaults.pcm { min/d; s/.*pcm.0.*/pcm.0{type hw card 0}/' $alsa_conf.{min,mix,plug}
+sudo sed -i '/defaults.pcm { min/d; s/.*pcm.0.*/pcm.!0{type hw card 0 device 0 subdevice 0}/' $alsa_conf.{min,mix,plug}
 [ $(grep mixer_type $mpd_conf | wc -l) -eq 1 ] && sudo sed -i '/buffer_before_play/amixer_type "none"' $mpd_conf
 sudo sed -i 's/ctl.9/ctl.hw/' $mpd_conf $rc_local
 sudo sed -i '/mpd)\|io)\|decoder)/s/._task/s_task/; /decoder)/s/._task/m_task/' $rc_local
