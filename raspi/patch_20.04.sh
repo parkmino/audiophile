9b420eabf59bf92ac0d058362c0e7081852e3c4e
Min-ho Park <parkmino@gmail.com>
2020-03-31 13:01:26 +0900

    Add bracket

--- raspi/patch_20.04.sh
+++ raspi/patch_20.04.sh
@@ -19,7 +19,7 @@ rc_local=/etc/rc.local
 sudo sed -i '/defaults.pcm { min/d; s/.*pcm.0.*/pcm.!0{type hw card 0 device 0 subdevice 0}defaults.pcm{minperiodtime 2774}/' $alsa_conf.{min,mix,plug}
 [ $(grep mixer_type $mpd_conf | wc -l) -eq 1 ] && sudo sed -i '/buffer_before_play/amixer_type "none"' $mpd_conf
 sudo sed -i 's/ctl.9/ctl.hw/' $mpd_conf $rc_local
-sudo sed -i '/mpd)/s/._task/h_task/; /io\|decoder\|player)/s/._task/s_task/' $rc_local
+sudo sed -i '/mpd)/s/._task/h_task/; /io)\|decoder)\|player)/s/._task/s_task/' $rc_local
 
 #kill $!
 #echo
