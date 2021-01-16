4c362c986dba2ea98bd64ad1caeb9207ca257a4e
Min-ho Park <parkmino@gmail.com>
2021-01-16 16:27:07 +0900

    Shorten URL

--- artik710/patch210116.sh
+++ artik710/patch210116.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #bash <(curl -s  https://raw.githubusercontent.com/parkmino/audiophile/master/artik710/patch210116.sh)
-#bash <(curl -sL https://bit.ly/...)
+#bash <(curl -sL https://bit.ly/35NVGwq)
 
 [ ! -d /root/copy ] && mkdir /root/copy
 wget https://launchpad.net/~jean-francois-dockes/+archive/ubuntu/upnpp1/+files/libnpupnp1_4.0.14-1~ppa1~xenial1_arm64.deb https://launchpad.net/~jean-francois-dockes/+archive/ubuntu/upnpp1/+files/libupnpp6_0.20.1-1~ppa1~xenial1_arm64.deb https://launchpad.net/~jean-francois-dockes/+archive/ubuntu/upnpp1/+files/upmpdcli-hra_1.5.6-1~ppa2~xenial1_all.deb https://launchpad.net/~jean-francois-dockes/+archive/ubuntu/upnpp1/+files/upmpdcli-qobuz_1.5.6-1~ppa2~xenial1_all.deb https://launchpad.net/~jean-francois-dockes/+archive/ubuntu/upnpp1/+files/upmpdcli-uprcl_1.5.6-1~ppa2~xenial1_all.deb https://launchpad.net/~jean-francois-dockes/+archive/ubuntu/upnpp1/+files/upmpdcli_1.5.6-1~ppa2~xenial1_arm64.deb -P /root/copy/
