17eedd3c2a525b4955f13959dd02535fba9cce10
Min-ho Park <parkmino@gmail.com>
2021-01-22 19:19:57 +0900

    Manual RoonBridge install

--- artik710/roonup.sh
+++ artik710/roonup.sh
@@ -1,23 +1,20 @@
 #!/bin/sh
 
-#bash <(curl -s  https://github.com/parkmino/audiophile/raw/master/artik710/roonup.sh)
-#bash <(curl -sL https://bit.ly/34RPSl0)
+# bash <(curl -s  https://raw.githubusercontent.com/parkmino/audiophile/master/artik710/roonup.sh)
+# bash <(curl -sL https://bit.ly/34RPSl0)
 
-dot () {
- while true; do
- printf .
- sleep 0.476
- done
-}
+#sed -i 's/>\/dev\/null<\/dev\/null 2>\/dev\/null/1<\/dev\/null 2<\/dev\/null 0<\/dev\/null/g' /opt/RoonBridge/Bridge/RoonBridge
 
-#dot &
+mv /opt/RoonBridge/Bridge/RoonBridge /opt/RoonBridge/Bridge/RoonBridge.tweak
 
-sudo wget http://download.roonlabs.com/builds/roonbridge-installer-linuxarmv8.sh -P /root/copy/
-sudo chmod 755 /root/copy/roonbridge-installer-linuxarmv8.sh
-sudo /root/copy/roonbridge-installer-linuxarmv8.sh
-sudo systemctl disable roonbridge
+[ ! -d /root/copy ] && mkdir /root/copy
+wget -O http://download.roonlabs.com/builds/RoonBridge_linuxarmv8.tar.bz2 -P /root/copy/
+systemctl stop roonbridgetoram
+tar -xjfv /root/copy/RoonBridge_linuxarmv8.tar.bz2 -C /opt/RoonBridge/
+strip --strip-debug /opt/RoonBridge/Bridge/processreaper
+find /opt/RoonBridge/ -name *.so -exec chmod -x {} \; -exec strip --strip-debug {} 2>/dev/null \;
 
-#kill $!
-#echo
+mv /opt/RoonBridge/Bridge/RoonBridge       /opt/RoonBridge/Bridge/RoonBridge.orig
+mv /opt/RoonBridge/Bridge/RoonBridge.tweak /opt/RoonBridge/Bridge/RoonBridge
 
 printf "* Finished and reboot to take effect. \n* 완료되어 시스템을 다시 시작하면 적용됩니다. (^_^)\n"
