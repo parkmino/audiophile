5a5acaae506cd55a5ae77c5e6640035c9fd299b3
parkmino <parkmino@gmail.com>
2021-07-10 00:31:14 +0900

    Remove unnecessary line

--- artik710/roonup.sh
+++ artik710/roonup.sh
@@ -3,8 +3,6 @@
 # bash <(curl -s  https://raw.githubusercontent.com/parkmino/audiophile/master/artik710/roonup.sh)
 # bash <(curl -sL https://bit.ly/34RPSl0)
 
-#sed -i 's/>\/dev\/null<\/dev\/null 2>\/dev\/null/1<\/dev\/null 2<\/dev\/null 0<\/dev\/null/g' /opt/RoonBridge/Bridge/RoonBridge
-
 mv /opt/RoonBridge/Bridge/RoonBridge /opt/RoonBridge/Bridge/RoonBridge.tweak
 
 [ ! -d /root/copy ] && mkdir /root/copy
