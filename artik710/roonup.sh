3a8fb119726656ea2c725c235608e4b6fc6987b8
parkmino <parkmino@gmail.com>
2021-07-10 07:35:13 +0900

    Update

--- artik710/roonup.sh
+++ artik710/roonup.sh
@@ -3,15 +3,12 @@
 # bash <(curl -s  https://raw.githubusercontent.com/parkmino/audiophile/master/artik710/roonup.sh)
 # bash <(curl -sL https://bit.ly/34RPSl0)
 
-#sed -i 's/>\/dev\/null<\/dev\/null 2>\/dev\/null/1<\/dev\/null 2<\/dev\/null 0<\/dev\/null/g' /opt/RoonBridge/Bridge/RoonBridge
-
 mv /opt/RoonBridge/Bridge/RoonBridge /opt/RoonBridge/Bridge/RoonBridge.tweak
-
 [ ! -d /root/copy ] && mkdir /root/copy
-wget -O http://download.roonlabs.com/builds/RoonBridge_linuxarmv8.tar.bz2 -P /root/copy/
+wget -O /root/copy/RoonBridge_linuxarmv8.tar.bz2 http://download.roonlabs.com/builds/RoonBridge_linuxarmv8.tar.bz2
 sync
 systemctl stop roonbridgetoram
-tar -xjfv /root/copy/RoonBridge_linuxarmv8.tar.bz2 -C /opt/RoonBridge/
+tar -xvf /root/copy/RoonBridge_linuxarmv8.tar.bz2 -C /opt/
 strip --strip-debug /opt/RoonBridge/Bridge/processreaper
 find /opt/RoonBridge/ -name *.so -exec chmod -x {} \; -exec strip --strip-debug {} 2>/dev/null \;
 
