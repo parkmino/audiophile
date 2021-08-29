157dd4584b2e43042bfcf972cd8f92a9a37460b1
parkmino <parkmino@gmail.com>
2021-08-29 20:52:37 +0900

    Update libreelec 10.0.0 for orange pi pc

--- libreelec/opipc/autostart.sh
+++ libreelec/opipc/autostart.sh
@@ -1,22 +1,36 @@
 #!/bin/sh
 
-[ -d /storage/.kodi/userdata/playlists/music/ ] && ! ls /storage/.kodi/userdata/playlists/music/*.pls>/dev/null 2>&1 && cp /etc/*.pls /storage/.kodi/userdata/playlists/music/
+[ -d /storage/.kodi/userdata/playlists/music/ ] && ! ls /storage/.kodi/userdata/playlists/music/*.pls>/dev/null 2>&1 && cp /etc/*.pls /storage/.kodi/userdata/playlists/music/ && cp /etc/playlist.m3u /storage/.kodi/userdata/playlists/mixed/
 
-[ -r /storage/.config/audio.conf ] && . /storage/.config/audio.conf
+[ -r /storage/.config/kodi.conf ] && . /storage/.config/kodi.conf
 
-cp /usr/lib/libasound.so.2.0.0.min /dev/shm/libasound.so.2.0.0
-cp /usr/bin/nohup /dev/shm/
+cp /usr/share/alsa/alsa.${alsa_output}.conf.min /dev/shm/alsa.conf
 
-if [ "$alsa_output" = hdmi ]; then
- cp /usr/share/alsa/alsa.hdmi.conf.min /dev/shm/alsa.conf
+if   [ "$alsa_output" = hdmi ]; then
+ for i in $(ls /dev/snd/); do
+  [ ! "$i" = "pcmC0D0p"  ] && [ ! "$i" = "controlC0"  ] && rm -rf "/dev/snd/$i"
+ done
+elif [ "$alsa_output" = int  ]; then
+ for i in $(ls /dev/snd/); do
+  [ ! "$i" = "pcmC1D0p"  ] && [ ! "$i" = "controlC1"  ] && rm -rf "/dev/snd/$i"
+ done
 else
- cp /usr/share/alsa/alsa.conf.min      /dev/shm/alsa.conf
+ for i in $(ls /dev/snd/); do
+  [ ! "$i" = "pcmC2D0p"  ] && [ ! "$i" = "controlC2"  ] && rm -rf "/dev/snd/$i"
+ done
 fi
-rm /dev/snd/hw* /dev/snd/seq /dev/snd/timer || true
 
-#echo 1000000 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate || true
-#echo 1 > /sys/devices/system/cpu/cpufreq/ondemand/ignore_nice_load || true
-#echo 100 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor || true
+#echo 1000000 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate        || true
+#echo 1 >       /sys/devices/system/cpu/cpufreq/ondemand/ignore_nice_load     || true
+#echo 100 >     /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor || true
+
+for i in $(ls /sys/block/*/queue/scheduler); do
+ if   echo $(cat $i) | grep -q none; then
+  echo none > $i
+ elif echo $(cat $i) | grep -q noop; then
+  echo noop > $i
+ fi
+done
 
 for i in $(ps -eo pid,class,ni,comm | grep -i TS | awk '$3 < 0 {print $1}'); do
  renice -2 $i || true
@@ -27,15 +41,14 @@ for i in $(ps -eo pid,class,comm | grep -E '(FF|RR)' | awk '$3 !~ /migration|mpd
  renice  -3 $i || true
 done
 
-m_task=3 # grep -m1 siblings /proc/cpuinfo | grep -o [0-9*] # getconf _NPROCESSORS_ONLN # echo $(($(cat /sys/devices/system/cpu/present | sed 's/0-//')+1)) # grep -c ^processor /proc/cpuinfo
-[ "$m_task" -ge 3 ] && s_task=$((m_task-2)) || s_task=0
-
-if [ "$m_task" -ge 1 ]; then
- for pid in $(ps -eo pid,comm | awk '$2 !~ /mpd|systemd$|kodi|kodi.bin/ {print $1}'); do
-  taskset -acp 0 $pid 2>/dev/null || true
- done
-fi
-
+m_task=2; s_task=0; h_task=3; o_task=1,3 # grep -m1 siblings /proc/cpuinfo | grep -o [0-9*] # getconf _NPROCESSORS_ONLN # echo $(($(cat /sys/devices/system/cpu/present | sed 's/0-//')+1)) # grep -c ^processor /proc/cpuinfo
+#
+#if [ "$m_task" -ge 1 ]; then
+# for pid in $(ps -eo pid,comm | awk '$2 !~ /mpd|systemd$|kodi|kodi.bin/ {print $1}'); do
+#  taskset -acp $o_task $pid 2>/dev/null || true
+# done
+#fi
+#
 #avail_gov=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors)
 #$(echo $avail_gov | grep -q ondemand)     && gov=ondemand
 #$(echo $avail_gov | grep -q conservative) && gov=conservative
@@ -46,10 +59,10 @@ fi
 #for i in $(seq 2 5); do
 # [ usb$i = off ] && hub-ctrl -h 0 -P $i -p 0
 #done
-#[ -n "$usb" ] && uhubctl -a 0 -p "$usb"
+[ "$usb" = "off" ] || uhubctl -a off -p 0
 
 ### Remove modules
-#modprobe -r 8021q || true
+ modprobe -r 8021q || true
 #modprobe -r bcm2835_gpiomem || true
 #modprobe -r fixed || true
 
@@ -57,35 +70,40 @@ swapoff -a
 
 echo 4 > /proc/irq/default_smp_affinity || true
 
-if [ "$alsa_output" = hdmi ]; then
- rm -rf /dev/snd/*c /dev/snd/by* /dev/snd/*C1* || true
-else
- rm -rf /dev/snd/*c /dev/snd/by* /dev/snd/*C0* /dev/snd/*C1* || true
-fi
-
+#[ "$alsa_conf" = min ] && rm -rf /dev/snd/*c /dev/snd/by* || true
 [ "$lirc"  = off ] && systemctl stop eventlircd
 [ "$nfs"   = off ] && systemctl stop rpcbind
 [ "$pulse" = off ] && systemctl stop pulseaudio
+[ "$vpn"   = off ] && systemctl stop connman-vpn
 [ "$wpa"   = off ] && systemctl stop wpa_supplicant
 
 (
-#until [ $(pgrep kodi.bin) -gt 0 ] 2>/dev/null && $(pstree -p | grep -q complet); do
- until [ $(pgrep kodi.bin) -gt 0 ] 2>/dev/null; do
+ until [ $(pgrep kodi.bin) -gt 0 ] 2>/dev/null && $(pstree -p | grep -q ActiveAE); do
   sleep 1
  done
- sleep 5
+ sleep 1
 
- pgr_kodi=$(pgrep kodi.bin)
- ppid=$pgr_kodi
+ #pgr_kodi=$(pgrep kodi.bin)
+ #ppid=$pgr_kodi
+ #
+ #until [ $ppid -eq 1 ]; do
+ # ppid=$(ps -o ppid= -p $ppid)
+ # taskset -cp 0,1,3 $ppid
+ #done
 
- until [ $ppid -eq 1 ]; do
-  ppid=$(ps -o ppid= -p $ppid)
-  taskset -cp $m_task $ppid
- done
+ #taskset -acp 0,1,3 $pgr_kodi
+ #for i in $(pstree -p $pgr_kodi | grep ActiveAE | cut -d "}" -f2 | cut -d "(" -f2 | cut -d ")" -f1); do
+ # taskset -cp $m_task $i
+ #done
 
- taskset -cp $m_task $pgr_kodi
+ if [ "$tweak" = on ]; then
+  systemctl stop systemd-journald systemd-logind || true
+  pkill hciattach || true
+  for i in $(ls /proc/sys/net/ipv4/conf/*/promote_secondaries); do
+   echo 0 > $i
+  done
+ fi
 
-#llctl f0 l0 d0
  echo none > /sys/class/leds/orangepi\:green\:pwr/trigger
  echo 0    > /sys/class/leds/orangepi\:green\:pwr/brightness
 ) &
