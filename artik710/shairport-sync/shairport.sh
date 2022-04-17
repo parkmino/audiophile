74cc40eeb02b7a7219da9477c6f17bc5742adaf4
parkmino <parkmino@gmail.com>
2022-04-17 22:21:51 +0900

    Make it executable

--- artik710/shairport-sync/shairport.sh
+++ artik710/shairport-sync/shairport.sh
@@ -7,6 +7,7 @@ apt-get update
 apt-get install libconfig9
 
 wget -O /usr/local/bin/shairport-sync https://github.com/parkmino/audiophile/raw/master/artik710/shairport-sync/shairport-sync
+chmod +x /usr/local/bin/shairport-sync
 wget -O /etc/shairport-sync.conf https://github.com/parkmino/audiophile/raw/master/artik710/shairport-sync/shairport-sync.conf
 getent group shairport-sync &>/dev/null || groupadd -r shairport-sync >/dev/null
 getent passwd shairport-sync &> /dev/null || useradd -r -M -g shairport-sync -s /usr/bin/nologin -G audio shairport-sync >/dev/null
