21899e2d7e0b3aa5e28582b96898449b6bd21d96
parkmino <parkmino@gmail.com>
2022-04-17 22:13:01 +0900

    Add sync

--- artik710/shairport-sync/shairport.sh
+++ artik710/shairport-sync/shairport.sh
@@ -12,8 +12,12 @@ getent group shairport-sync &>/dev/null || groupadd -r shairport-sync >/dev/null
 getent passwd shairport-sync &> /dev/null || useradd -r -M -g shairport-sync -s /usr/bin/nologin -G audio shairport-sync >/dev/null
 wget -O /lib/systemd/system/shairport-sync.service https://github.com/parkmino/audiophile/raw/master/artik710/shairport-sync/shairport-sync.service
 
+sync
+
 systemctl disable shairport-sync
 systemctl daemon-reload
 
 grep -q shairport /etc/default/audio.conf || sed -i 'ashairportt=on' /etc/default/audio.conf
 grep -q shairport /etc/rc.local || sed -i '/$naa/a\\n[ "$shairport" = on ] && cp /usr/local/bin/shairport-sync /dev/shm/ && systemctl start shairport-sync' /etc/rc.local
+
+sync
