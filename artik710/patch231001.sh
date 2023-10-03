61e44f962ee0c072cfd6e57224d89f2a0f2bcd59
parkmino <parkmino@gmail.com>
2023-10-03 20:07:50 +0900

    Add another example

--- artik710/patch231001.sh
+++ artik710/patch231001.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-#bash <(curl -sL https://github.com/parkmino/audiophile/raw/master/artik710/patch231001.sh)
-#bash <(curl -sL https://bit.ly/3LKaUHN)
+## bash <(curl -sL https://github.com/parkmino/audiophile/raw/master/artik710/patch231001.sh)
+## bash <(curl -sL https://bit.ly/3LKaUHN)
+#$ curl -sL https://bit.ly/3LKaUHN | sudo bash
 
 wget -P /etc/ https://github.com/parkmino/audiophile/raw/master/sh/kr2mpd
 chmod a+rx /etc/kr2mpd
