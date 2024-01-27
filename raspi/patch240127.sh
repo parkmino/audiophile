ea28d08cf19ba0217dfc627e10a08fdf142b1682
parkmino <parkmino@gmail.com>
2024-01-27 19:39:00 +0900

    Fix url

--- raspi/patch240127.sh
+++ raspi/patch240127.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-## bash <(curl -sL https://github.com/parkmino/audiophile/raw/master/raspi/patch241027.sh)
-## bash <(curl -sL https://bit.ly/3HAP0Es)
-#$ curl -sL https://bit.ly/3HAP0Es | sudo bash
+## bash <(curl -sL https://github.com/parkmino/audiophile/raw/master/raspi/patch240127.sh)
+## bash <(curl -sL https://bit.ly/3Oj0wZ2)
+#$ curl -sL https://bit.ly/3Oj0wZ2 | sudo bash
 
 wget https://github.com/parkmino/audiophile/raw/master/raspi/kr2mpd
 chmod a+rx kr2mpd
